4219249beb7dfedbe82050bcba94a962656d966b
Zimin7941VC <zimin008@rambler.ru>
2021-12-28 22:13:09 +0300

    Добавили меню и задание

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+﻿#include <fstream>
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	ifstream fin("text.txt", ios::in);
+	if (!fin)
+	{
+		cout << "Oshibka otkritija faila" << endl;
+		return 1;
+	}
+	fin.seekg(0, ios::end);
+	long len = fin.tellg();
+	char *buf = new char[len + 1];
+	fin.seekg(0, ios::beg);
+	fin.read(buf, len);
+	buf[len] = '\0';
+	long n = 0, i = 0, j = 0, flag = -1;
+	while (buf[i])
+	{
+		if (buf[i] == '"')
+			if (flag < 0)
+				flag = i;
+			else
+			{
+				for (j = flag; j <= i; j++)
+					cout << buf[j];
+				flag = -1;
+			}
+
+
+		if (buf[i] == '.' || buf[i] == '!')
+			n = i + 1;
+		i++;
+	}
+	fin.close();
+	cout << endl;
+	return 0;
+}
---
+++ Laba_2_TP.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+#include "TrainKeeper.h"
+
+void printFirstInformation() {
+	std::cout << "\nWant you want to do?\n";
+	std::cout << "1. Add Train\n";
+	std::cout << "2. Display Timeofdispatch and train numbers\n";
+	std::cout << "3. Exit\n";
+}
+
+
+
+int main() {
+	TrainKeeper trainKeeper;
+
+	int choiser;
+	std::cout << "Hello!\n";
+
+	while (true) {
+		printFirstInformation();
+		std::cout << "Write a number: ";
+		std::cin >> choiser;
+		std::cout << "\n";
+		if (choiser == 3) {
+			std::cout << "Bye\n";
+			break;
+		}
+		try {
+			trainKeeper.dataProcessing(choiser);
+		}
+		catch (std::invalid_argument error) {
+			std::cerr << error.what();
+		}
+	}
+
+	return 0;
+}
